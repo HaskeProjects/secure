6a84c075011b465b4838dbce1b7f591538d95d39
devsamahd <devsamahd@gmail.com>
2023-03-20 00:55:33 +0100

    ..

--- models/residents.model.js
+++ models/residents.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const reSchema = new mongoose.Schema({
-        name: {type:String, required:true},
+        firstname: {type:String, required:true},
+        lastname: {type:String, required:true}, 
         esId: {type: mongoose.Types.ObjectId},
         apartment: {type:String, required: true},
         number: {type:Number, require:true}
