4bcb7ed8db0b7df0afa5453b2d0cd31b9f850efb
devsamahd <devsamahd@gmail.com>
2023-03-20 00:54:43 +0100

    .

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require('express')
 const App = express()
 const cors = require('cors')
 require('dotenv').config()
+require('./lib/db')()
 const PORT = 330 || process.env.PORT
-cors()
+App.use(cors())
+App.use(express.json())
 
-App.get('/',(req,res)=>{return res.json({message:"Connect Yay"})})
+App.get('/', (req,res)=>{return res.json({message:"Connect Yay"})})
 
+App.use('/es', require('./routes/estates.routes'))
+App.use('/chair', require('./routes/chairman.routes'))
 
 App.listen(PORT, (err)=>{
     if(err) throw err
-    console.log(green, "connected successfully on "+PORT)
+    console.log("\u001b[34m connected successfully on "+PORT)
 })
