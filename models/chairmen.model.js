d1066aa92f38249e194787f616f506d4358e376f
devsamahd <devsamahd@gmail.com>
2023-03-23 19:24:43 +0100

    ..

--- models/chairmen.model.js
+++ models/chairmen.model.js
@@ -5,8 +5,7 @@ const chSchema = new mongoose.Schema({
         esId: {type: mongoose.Types.ObjectId},
         number: {type:Number, require:true},
         password: {type:String, required: true},
-        email: {type:String, required: true},
-        accessToken: {type:String, required: true, default: "''"}
+        email: {type:String, required: true}
 },{
         timestamps:true,
         toJSON:{virtuals:true},
