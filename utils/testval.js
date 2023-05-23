4630861ff483c2d6689d186a02c6310dcfeff71d
HaskeProjects <129946924+HaskeProjects@users.noreply.github.com>
2023-05-23 22:43:17 +0100

    Update testval.js

--- utils/testval.js
+++ utils/testval.js
@@ -1,9 +1,7 @@
 const moment = require('moment')
 const testval = (end) => {
-    const today = moment().format('DD-MM-YYYY')
-    const diff = moment(end, 'DD-MM-YYYY').isAfter(today)
-    console.log(diff, end, today)
-    return diff
+    const today = moment().format('DD-MM-YYYY HH:mm:ss')
+    const diff2 = end > today
+    return diff2
 }
-
 module.exports = testval
