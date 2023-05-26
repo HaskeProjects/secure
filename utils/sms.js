4ed890da31393a16a9d8258b5a58f996949ee243
HaskeProjects <129946924+HaskeProjects@users.noreply.github.com>
2023-05-26 17:10:53 +0100

    Update sms.js

--- utils/sms.js
+++ utils/sms.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 const sendSMS = async(num, message) => {
-    const url = `https://app.multitexter.com/v2/app/sms?email=eaogolekwu@gmail.com&password=multitxt1x@&message=${message}&sender_name=ResidentPro&recipients=${num}&forcednd=1`
+    const url = `https://api.bulksmslive.com/v2/app/sms?email=eaogolekwu@gmail.com&password=smslive1x@@&message=${message}&sender_name=ResidentPro&recipients=${num}&forcednd=1`
     try{
         const tr = await fetch(url)
         const res = await tr.json()
