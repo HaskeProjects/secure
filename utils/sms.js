1c818ba4f293a5c0f96b4cbde29317182ee1d9d4
HaskeProjects <129946924+HaskeProjects@users.noreply.github.com>
2023-05-28 12:24:11 +0100

    Update sms.js

--- utils/sms.js
+++ utils/sms.js
@@ -9,29 +9,27 @@ const sendSMS = async(num, message) => {
 //         return res.status(403).json({message:e})
 //         console.log(e)
 //     }
-    
-    fetch('https://www.bulksmsnigeria.com/api/v2/sms', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        body: message,
-        from: "ResidentPro",
-        to: num,
-        api_token: "H0SJvKTktd0S1BNw03RxXGCXlaSgBbk4V0c2dGGPljXSg6goc7tNapPnaB0j",
-        gateway: "direct-refund"
-      })
-    })
-      .then(response => response.json())
-      .then(data => {
+    try{
+        const mes = await fetch('https://www.bulksmsnigeria.com/api/v2/sms', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            body: message,
+            from: "ResidentPro",
+            to: num,
+            api_token: "H0SJvKTktd0S1BNw03RxXGCXlaSgBbk4V0c2dGGPljXSg6goc7tNapPnaB0j",
+            gateway: "direct-refund"
+          })
+        })
+        const res = await mes.json()
         return res
-      })
-      .catch(e => {
-        console.log(error);
-        return res.status(403).json({message:e})
-      });
+    }catch(e){
+    console.log(error);
+    return res.status(403).json({message:e})
+  }
 }
 
 module.exports = sendSMS
