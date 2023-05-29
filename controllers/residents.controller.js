6e42ee616ea40a363e81b7d5c9bb0e2b3146aa51
HaskeProjects <129946924+HaskeProjects@users.noreply.github.com>
2023-05-29 16:58:09 +0100

    Update residents.controller.js

--- controllers/residents.controller.js
+++ controllers/residents.controller.js
@@ -34,7 +34,8 @@ const requestRat = async(req, res) => {
     const test = testval(est.end)
     if(!test) return res.status(403).json({message:'Estate Inactive'})
     const token = randomizer.generate({length:4, charset: 'hex',capitalization: 'uppercase'})+randomizer.generate({length:1,charset: 'alphabetic',capitalization: 'uppercase'})+randomizer.generate({length:1,charset: 'number',capitalization: 'uppercase'})
-    const mes = `From ResidentProtect: Your Resident Access Token is ${token}. Do not disclose this to anyone. \n ResidentProtect is an engineered technology built to protect residents of gated estate.`
+    const mes = `From ResidentProtect: Your Resident Pass: ${token}. Do not disclose this to anyone. \n ResidentProtect is an engineered technology built to protect residents of gated estate.`
+    
     const message = await sendSMS(number, mes)
     if(message){
         found.crat = token
